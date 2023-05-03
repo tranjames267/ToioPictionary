e6d4f841cc13d6d3572c1a348768f2e34475fa19
tranjames267 <58448725+tranjames267@users.noreply.github.com>
2023-05-02 22:20:48 -0600

    Added more words

--- sketch.js
+++ sketch.js
@@ -6,18 +6,17 @@ let i = 0;
 function setup() {
   createCanvas(500, 500);
   background(235);
-  
+
   // let col = color(220, 245, 250);
-  button = createButton('Prompt');
+  button = createButton("Prompt");
   button.position(420, 460);
   button.size(70, 30);
   // button.style('background-color', col);
-  
+
   button.mousePressed(pickWord);
 }
 
 function pickWord() {
-  
   let words = [];
   words = [
     "Apple",
@@ -30,28 +29,43 @@ function pickWord() {
     "Computer",
     "Chair",
     "Lightbulb",
-  ]
-  
+    "Tornado",
+    "Flower",
+    "Carrot",
+    "Comet",
+    "Hat",
+    "Candy",
+    "Rainbow",
+    "Moon",
+    "Sun",
+    "Tree",
+    "Cloud",
+    "Banana",
+    "Socks",
+    "Butterfly",
+    "Pizza",
+  ];
+
   rectMode(CENTER);
   noStroke();
   fill(0, 174, 202);
-  rect(255,10,500,120); 
-  
+  rect(250, 10, 500, 120);
+
   let word = random(words); // select random word
-  textAlign(CENTER); 
+  textAlign(CENTER);
   textSize(30);
-  fill('white');
+  fill("white");
   text(word, 250, 45);
   console.log(i);
   i++;
-  
-  if (i%2 == 0) {
+
+  if (i % 2 == 0) {
     createCanvas(500, 500);
-    background(235); 
+    background(235);
     rectMode(CENTER);
     noStroke();
     fill(0, 174, 202);
-    rect(255,10,500,120);
+    rect(250, 10, 500, 120);
   }
 }
 
@@ -63,39 +77,34 @@ function draw() {
 
   // Keep on chasing the other Cube
   const moveType = P5tCube.moveTypeId.withoutBack;
-  const spd = 20;
-  
+  const spd = 80;
+
   cubeQ?.moveToCube(cubeP, spd, moveType);
-  
-  
-  if(onMat === true){
+
+  if (onMat === true) {
     noStroke();
-    fill('black');   
-    ellipse(cubeP.x, cubeP.y, 10, 10);      
+    fill("black");
+    ellipse(cubeP.x, cubeP.y, 10, 10);
   }
-  
+
   const type = "buttonpress";
   cubeP?.addEventListener(type, () => {
-    
     cubeQ?.playMelody([
-      
       { note: 0x3c, duration: 0x10 },
       { note: 0x40, duration: 0x10 },
       { note: 0x43, duration: 0x10 },
       { note: 0x48, duration: 0x10 },
     ]);
   });
-   
 }
 
 function keyPressed() {
   P5tCube.connectNewP5tCube().then((cube) => {
     connectedCubeArray.push(cube);
     cube.turnLightOn("white");
-    onMat = true; 
+    onMat = true;
   });
 }
-
 
 //Reference
 // cubeP?.moveToCube(cubeQ, spd, moveType);
@@ -108,11 +117,11 @@ function keyPressed() {
 // cubeP?.turnLightOn( 'green' );
 // cubeP?.turnLightOn( 'blue' );
 // cubeP?.turnLightOn( 'violet' );
-// cubeP?.turnLightOff(); 
-    
+// cubeP?.turnLightOff();
+
 // C D E F G A B C
 //     cubeP?.playMelody([
-      
+
 //       { note: 0x3c, duration: 0x1e },
 //       { note: 0x3e, duration: 0x1e },
 //       { note: 0x40, duration: 0x1e },
@@ -122,5 +131,3 @@ function keyPressed() {
 //       { note: 0x47, duration: 0x1e },
 //       { note: 0x48, duration: 0x1e },
 //     ]);
-
-
